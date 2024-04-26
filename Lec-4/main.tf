ce06026aacdc11b47236bcc833046e0dbb33b1f3
Raghunath Kushawaha <78621381+iamraghu149@users.noreply.github.com>
2024-04-26 11:15:19 +0530

    Updated by Raghu ReadME
    Updated by Raghu ReadME

--- Lec-4/main.tf
+++ Lec-4/main.tf
@@ -1,1 +1,25 @@
+resource "aws_instance" "Web-Server" {
+  ami = var.os #ubuntu
+  instance_type = var.size
 
+  tags = {
+    Name = var.name
+  }
+}
+
+# Create S3 Bucket
+resource "aws_s3_bucket" "bucket" {
+  bucket = var.bucketname
+}
+
+resource "aws_iam_user" "myusers" {
+  name = "${var.username}-user"
+}
+
+#output "IPAddress" {
+  #value = aws_instance.Web-Server.public_ip
+#}
+
+output "DNS" {
+  value = aws_instance.Web-Server.public_dns
+}
