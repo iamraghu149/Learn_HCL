a1dc5ba2ebb99841ef2cefbf1d4999e16146467e
Raghunath Kushawaha <78621381+iamraghu149@users.noreply.github.com>
2024-04-26 11:17:20 +0530

    Updated by Raghu ReadME

---
+++ Lec-4/variable.tf
@@ -0,0 +1,21 @@
+variable "os" {
+  type = string
+  default = "ami-007020fd9c84e18c7"
+  description = "This is my AMI ID"
+}
+
+variable "size" {
+  default = "t2.micro"
+}
+
+variable "name" {
+  default = "TerraformEC2"
+}
+
+variable "bucketname" {
+  
+}
+
+variable "username" {
+  
+}
